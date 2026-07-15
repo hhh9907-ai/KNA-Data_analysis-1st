f77b863013f0aef235c3f171a6e608e49db78252
hhh9907-ai <hhh9907@gmail.com>
2026-07-15 14:12:45 +0900

    변수 변수

---
+++ 04_variable.py
@@ -0,0 +1,81 @@
+# # temp = 36
+# # name = "센서 A"
+
+# # print(temp)
+# # print(name)
+
+# # x = 5
+# # x = x + 5
+# # print(x)
+
+# # temp = 10
+# # print(temp)
+# # print("temp:", temp, "name", name)
+
+width, height = 10, 20
+print(width, height)
+
+
+# temp = 25
+# name = "센서A"
+
+# print(temp)
+# print(name)
+
+# temperature = 30
+# print(temperature)
+
+# my_number = 13
+# print(my_number)
+
+# mood = "쏘쏘"
+# print(mood)
+
+# age = 33
+# label = "나이"
+# print(label)
+# print(age)
+
+x = 10
+print(x)
+
+x = x + 5
+print(x)
+
+x = x * 2
+print(x)
+
+a = 100
+b = a 
+a = 999
+print(a)
+print(b)
+
+print(temp)
+temp = 25
+print(temp)
+
+score = 90
+print(Score)
+print(score)
+
+temp = 25
+count = 3
+#temp = 100
+print(temp)
+
+x = 5
+x = 10
+x = x + 1
+print(x)
+
+name = "센서A"
+temp = 25
+print("설비")
+print(name)
+print(temp)
+
+device_temp = 16
+sensor_count = 10
+print(device_temp) 
+print(sensor_count) 
