ead7d701db94940d8d6b216cd42c43f22ac638c4
hhh9907-ai <hhh9907@gmail.com>
2026-07-16 09:18:47 +0900

    study 비교연산자

---
+++ 06_operator_update.py
@@ -0,0 +1,37 @@
+print(3 + 5)
+print(10 - 4)
+print(3 * 8)
+print(10 / 4)
+print(10 // 4)
+print(10**4)
+print(80 % 5)
+
+a = 17
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a**b)
+
+x = 10
+y = 20
+z = 30
+print((x + y + z) / 3)
+
+# 정사각형 한변의 길이 : 10
+print(10 * 10)
+
+# 직사각형 변의 길이 가로 5 세로 10 높이 11
+print(5 * 10 * 11)
+
+print(7 < 16)
+print(7 > 16)
+print(7 <= 16)
+print(7 >= 16)
+print(7 == 16)
+print(7 != 16)
+
+print("hello" != "hello")  # False
